2fea862b27853ac93448c0a8954e13d1d511c56e
kantarellen <51992966+kantarellen@users.noreply.github.com>
2020-04-17 15:49:26 +0200

    add goBack function

--- js/main.js
+++ js/main.js
@@ -14,15 +14,15 @@ $(window).on("load",function() {
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade").each(function() {
-      /* Check the location of each desired element */
       var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+      if (objectBottom < windowBottom) { 
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
+      } else { 
         if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
       }
     });
-  }).scroll(); //invoke scroll-handler on page-load
+  }).scroll(); 
 });
+function goBack() {
+  window.history.back();
+}
